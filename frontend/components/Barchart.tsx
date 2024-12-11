71384caa49d32db6e27c5f46fe24088f73c12fba
Austin Huang <im@austinhuang.me>
2024-12-11 12:57:41 -0500

    feat: add animation to barchart

--- frontend/components/Barchart.tsx
+++ frontend/components/Barchart.tsx
@@ -26,15 +26,6 @@ function Barchart(props: {
         const width = canvasWidth - margin.left - margin.right;
         const height = props.height - margin.top - margin.bottom;
 
-        d3.select(ref.current)
-            .selectAll("*")
-            .remove();
-
-        // Create SVG container
-        const svg = d3.select(ref.current)
-        .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
-
         // Create scales
         const x = d3.scaleBand()
         .domain(top5Data.map(d => d.stock))
@@ -42,17 +33,32 @@ function Barchart(props: {
         .padding(0.1);
 
         const y = d3.scaleLinear()
-        .domain([0, d3.max(top5Data, d => d.holdingAmount)!])
+        .domain([0, d3.max(props.data, d => d.holdingAmount)!])
         .nice()
         .range([height, 0]);
 
-        // Add axes
-        svg.append("g")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(x));
+        // Create SVG container
+        if (d3.select(ref.current).select("#barchart_g").empty()) {
+            d3.select(ref.current)
+                .append("g")
+                .attr("id", "barchart_g")
+                .attr("transform", `translate(${margin.left},${margin.top})`)
+                .append("g")
+                .attr("id", "barchart_axes");
+        }
 
-        svg.append("g")
-        .call(d3.axisLeft(y));
+        const svg = d3.select(ref.current).select("#barchart_g");
+        const axesContainer = svg.select("#barchart_axes");
+
+        axesContainer.selectAll("g").remove();
+
+        // Add axes
+        axesContainer.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x));
+
+        axesContainer.append("g")
+            .call(d3.axisLeft(y));
 
         // Define a color scale
         const color = d3.scaleOrdinal<string>()
@@ -60,29 +66,27 @@ function Barchart(props: {
             .range(d3.schemeCategory10);
         
         // Draw bars with different colors
-        svg.selectAll(".bar")
+        svg
+        .selectAll("rect")
         .data(top5Data)
-        .enter()
-        .append("rect")
-        .attr("class", "bar")
+        .join(
+            enter => {
+                console.log(enter);
+                return enter.append("rect");
+            },
+            update => {
+                console.log(update);
+                return update;
+            },
+            exit => exit.remove()
+        )
+        .transition()
         .attr("x", d => x(d.stock)!)
         .attr("y", d => y(d.holdingAmount))
         .attr("width", x.bandwidth())
         .attr("height", d => height - y(d.holdingAmount))
         .attr("fill", (d) => color(d.stock)); // Assign color based on stock name
 
-        
-        // Draw bars
-        svg.selectAll(".bar")
-        .data(top5Data)
-        .enter()
-        .append("rect")
-        .attr("class", "bar")
-        .attr("x", d => x(d.stock)!)
-        .attr("y", d => y(d.holdingAmount))
-        .attr("width", x.bandwidth())
-        .attr("height", d => height - y(d.holdingAmount))
-        .attr("fill", "steelblue");
     }, [props.data, props.height, props.width, windowWidth]);
 
     return <svg width={canvasWidth} height={props.height} id="barchart" ref={ref} />;
